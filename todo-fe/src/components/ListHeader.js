2edeafc40938f1ee01eb3cc1d055ae273e8692da
Ejcodes <ihmdemon.exe@gmail.com>
2023-10-22 13:51:10 -0400

    added some css for the and fleshed out the button layout

--- todo-fe/src/components/ListHeader.js
+++ todo-fe/src/components/ListHeader.js
@@ -1,8 +1,16 @@
 
-const ListHeader = () => {
+const ListHeader = ({ listName }) => {
+
+  const signOut = () => {
+    console.log("signOut")
+    }
     return (
-      <div>
-         
+      <div className="list-header">
+        <h1>{listName}</h1>
+        <div className="button-container">
+          <button className="create">ADD NEW</button>
+          <button className="signout" onClick={signOut}>SIGN OUT</button>
+        </div>
       </div>
     );
   }
